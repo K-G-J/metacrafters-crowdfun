a39f0033f3ec754d5cd179110a055438eda26af8
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-16 15:30:04 -0500

    added modifiers and changed cancelled

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -1,24 +1,5 @@
-/**
- * 
-• Code compiles
-• Code accomplishes task described in prompt
-• Code has no glaring security issues
-• Code is readable and organized
-• Demonstrates ability to create and use modifiers appropriately
-• Demonstrates ability to create and emit events appropriately
-• Demonstrates ability to use contract inheritance appropriately
-• Demonstrates ability to validate conditions and throw sensible errors
-• Demonstrates ability to appropriately use global functions to access
-information about the transaction, block, address, etc.
-• Demonstrates ability to choose appropriate memory types for
-parameters, variables, etc.
-• Smart contract can quickly and easily be run on a local network
-• Project demonstrates understanding of common EVM developer
-tooling, e.g. truffle, ganache, hardhat, etc.
-• Contract is upgradeable
- */
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -36,6 +17,7 @@ contract Crowdfund is Initializable {
      * @member startAt - the block.timestamp at which the campaign will begin
      * @member endAt - the block.timestamp at which the campaign will be over (must be less than 90 days from when th creator calls the function to launch the campaign)
      * @member claimed - whether or not the crowdfunded tokens for the campaign have been claimed
+     * @member cancelled - whether or not the creator has cancelled the campaign
      */
 
     struct Campaign {
@@ -46,12 +28,22 @@ contract Crowdfund is Initializable {
         uint32 startAt;
         uint32 endAt;
         bool claimed;
+        bool cancelled;
     }
     /**
      * @dev custom ERC20 which funds take the form of
+     * Set in initalize function
      */
     using SafeERC20 for IERC20;
     IERC20 public token;
+
+    /**
+     * @dev maximum and minimum length of times a campaign can be
+     * Set in initalize function
+     */
+    uint256 public maxDuration;
+
+    uint256 public minDuration;
     /**
      * @dev used to track and update unique campaign ids
      */
@@ -82,28 +74,73 @@ contract Crowdfund is Initializable {
     event Claim(uint256 id);
     event Refund(uint256 indexed id, address indexed caller, uint256 amount);
 
+    /* ========== MODIFIERS ========== */
+
+    modifier campaignExists(uint256 _id) {
+        require(campaigns[_id].id != 0, "campaign does not exist");
+        _;
+    }
+
+    modifier notCancelled(uint256 _id) {
+        require(!campaigns[_id].cancelled, "campaign cancelled");
+        _;
+    }
+
+    modifier onlyCreator(uint256 _id) {
+        require(campaigns[_id].creator == msg.sender, "not creator");
+        _;
+    }
+
+    modifier campaignStarted(uint256 _id) {
+        require(
+            block.timestamp >= campaigns[_id].startAt,
+            "campaign not started"
+        );
+        _;
+    }
+
+    modifier campaignNotEnded(uint256 _id) {
+        require(block.timestamp <= campaigns[_id].endAt, "campaign ended");
+        _;
+    }
+
+    modifier campaignEnded(uint256 _id) {
+        require(block.timestamp > campaigns[_id].endAt, "campaign not ended");
+        _;
+    }
+
     /* ========== INITIALIZER ========== */
 
     /**
      * @dev dunction from @openzeppelin to initialize this as the base contract for upgrades
      * @dev must be the first function called
      */
-    
-    function initialize(address _token) public initializer {
-        token = IERC20(_token);
+
+    function initialize(
+        IERC20 _token,
+        uint256 _maxDuration,
+        uint256 _minDuration
+    ) public initializer {
+        token = _token;
+        maxDuration = _maxDuration;
+        minDuration = _minDuration;
     }
 
     /**
      * @dev function called by campaign creator to create the crowdfunding campaign and add Campaign to mapping
      * @param _goal - the amount of tokens the campaign aims to receive
      * @param _startAt - the time the campaign will begin (note: _startAt time is passed in by creator and not necessarily the time the creator calls the launch function)
-     * @param _endAt - the block.timestamp at which the campaign will end (note: must be less than 90 days from when the creator calls the launch function)
+     * @param _endAt - the block.timestamp at which the campaign will end (note: must be >= starting time + minDuration and <= starting time + maxDuration)
      */
 
     function launch(uint256 _goal, uint32 _startAt, uint32 _endAt) external {
         require(_startAt >= block.timestamp, "start at < now");
-        require(_endAt >= _startAt, "end at < start at");
-        require(_endAt <= block.timestamp + 90 days, "end at > max duration");
+        require(
+            _endAt >= _startAt + minDuration &&
+                _endAt <= _startAt + maxDuration,
+            "not in min & max duration"
+        );
+
         count += 1;
         campaigns[count] = Campaign({
             id: count,
@@ -112,22 +149,24 @@ contract Crowdfund is Initializable {
             pledged: 0,
             startAt: _startAt,
             endAt: _endAt,
-            claimed: false
+            claimed: false,
+            cancelled: false
         });
+
         emit Launch(count, msg.sender, _goal, _startAt, _endAt);
     }
 
     /**
      * @dev function called by campaign creator to cancel a crowdfunding campaign
-     * @dev the campaign must not have already started
      * @param _id - the uinique uint256 id of the campaign to be cancelled
      */
-    function cancel(uint256 _id) external {
+    function cancel(
+        uint256 _id
+    ) external campaignExists(_id) notCancelled(_id) onlyCreator(_id) {
         Campaign memory campaign = campaigns[_id];
-        require(campaign.id != 0, "campaign does not exist");
-        require(msg.sender == campaign.creator, "not creator");
-        require(block.timestamp < campaign.startAt, "started");
-        delete campaigns[_id];
+
+        campaign.cancelled = true;
+
         emit Cancel(_id);
     }
 
@@ -137,27 +176,38 @@ contract Crowdfund is Initializable {
      * @param _id - the uinique uint256 id of the campaign to be pledged
      * @param _amount - the amount of tokens trasferred from pledger into contract
      */
-    function pledge(uint256 _id, uint256 _amount) external {
+    function pledge(
+        uint256 _id,
+        uint256 _amount
+    )
+        external
+        campaignExists(_id)
+        notCancelled(_id)
+        campaignStarted(_id)
+        campaignNotEnded(_id)
+    {
         Campaign storage campaign = campaigns[_id];
-        require(block.timestamp >= campaign.startAt, "not started");
-        require(block.timestamp <= campaign.endAt, "ended");
 
         campaign.pledged += _amount;
         pledgedAmount[_id][msg.sender] += _amount;
         token.safeTransferFrom(msg.sender, address(this), _amount);
+
         emit Pledged(_id, msg.sender, _amount);
     }
 
     /**
-     * @dev function for pledger to unpledge their tokens and receive them back before a campaign has ended
+     * @dev function for pledger to unpledge their tokens and receive them back BEFORE a campaign has ended
+     * @dev if a campaign has ended and not met goal, pledger then calls refund function
      * @param _id - the uinique uint256 id of the campaign to be unpledged
      * @param _amount - the amount of tokens transferred from contract back to pledger
      */
 
-    function unpledge(uint256 _id, uint256 _amount) external {
+    function unpledge(
+        uint256 _id,
+        uint256 _amount
+    ) external campaignExists(_id) campaignNotEnded(_id) {
         Campaign storage campaign = campaigns[_id];
         require(pledgedAmount[_id][msg.sender] <= _amount, "invalid amount");
-        require(block.timestamp <= campaign.endAt, "ended");
 
         campaign.pledged -= _amount;
         pledgedAmount[_id][msg.sender] -= _amount;
@@ -169,14 +219,15 @@ contract Crowdfund is Initializable {
 
     /**
      * @dev function called by campaign creator to receive funds
-     * @dev called must be creator, campaign must have passed end at time, the campaign goal must have been met or exceeded
+     * @dev called must be creator, creator cannot have cancelled the campaign, campaign must have passed end at time, the campaign goal must have been met or exceeded
      * @dev this function can only be called once by creator
      * @param _id - the uinique uint256 id of the campaign which funds are claimed
      */
-    function claim(uint256 _id) external {
+    function claim(
+        uint256 _id
+    ) external onlyCreator(_id) notCancelled(_id) campaignEnded(_id) {
         Campaign storage campaign = campaigns[_id];
-        require(msg.sender == campaign.creator, "not creator");
-        require(block.timestamp > campaign.endAt, "not ended");
+
         require(campaign.pledged >= campaign.goal, "pledged < goal");
         require(!campaign.claimed, "claimed");
 
@@ -188,14 +239,15 @@ contract Crowdfund is Initializable {
     }
 
     /**
-     * @dev function to refund tokens from this contract back to the pledger
+     * @dev function to refund tokens from this contract back to the pledger after a campaign fails
      * @dev when a funding goal is not met, pledgers are be able to get a refund of their pledged funds
      * @param _id - the uinique uint256 id of the campaign to refund from
      */
-    function refund(uint256 _id) external {
+    function refund(
+        uint256 _id
+    ) external campaignExists(_id) campaignEnded(_id) {
         Campaign storage campaign = campaigns[_id];
-        require(campaign.id != 0, "campaign does not exist");
-        require(block.timestamp > campaign.endAt, "not ended");
+
         require(campaign.pledged < campaign.goal, "pledged >= goal");
 
         uint256 bal = pledgedAmount[_id][msg.sender];
