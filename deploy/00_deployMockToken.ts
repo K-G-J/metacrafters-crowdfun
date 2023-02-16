ea748ef6f06aa7085a04d6f776a8c06aa6596d4f
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-16 16:50:39 -0500

    updated errors in deploy scripts

--- deploy/00_deployMockToken.ts
+++ deploy/00_deployMockToken.ts
@@ -10,36 +10,36 @@ const deployMockToken: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ) {
   // @ts-ignore
-  const { deployments, getNamedAccounts, network} = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
 
   const chainId = network.config.chainId;
 
   if (chainId == 31337) {
-    
     const waitBlockConfirmations = developmentChains.includes(network.name)
-    ? 1
-    : VERIFICATION_BLOCK_CONFIRMATIONS;
+      ? 1
+      : VERIFICATION_BLOCK_CONFIRMATIONS;
 
     log('----------------------------------------------------');
-    
-  const crowdfund = await deploy('Crowdfund', {
-    from: deployer,
-    log: true,
-    waitConfirmations: waitBlockConfirmations,
-  });
 
-  // Verify the deployment
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
-    log('Verifying...');
-    await verify(crowdfund.address, []);
+    const mockToken = await deploy('MockToken', {
+      from: deployer,
+      log: true,
+      waitConfirmations: waitBlockConfirmations
+    });
+
+    // Verify the deployment
+    if (
+      !developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+    ) {
+      log('Verifying...');
+      await verify(mockToken.address, []);
+    }
+    log('----------------------------------------------------');
   }
-  log('----------------------------------------------------');
-  };
-  
+};
+
 export default deployMockToken;
 deployMockToken.tags = ['all', 'MockToken'];
--- deploy/01_deployCrowdfund.ts
+++ deploy/01_deployCrowdfund.ts
@@ -1,16 +1,16 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { ethers } from "hardhat"
-import { BytesLike, BigNumber } from "ethers"
+import { ethers } from 'hardhat';
+import { BytesLike, BigNumber } from 'ethers';
 
 const developmentChains = ['hardhat', 'localhost'];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 import verify from '../utils/verify';
 
-const MIN_DURATION: BigNumber = ethers.BigNumber.from("30") // 30 seconds
-const MAX_DURATION = ethers.BigNumber.from("120") // two minutes
-const PLEDGE_AMOUNT = ethers.utils.parseEther("1");
+const MIN_DURATION: BigNumber = ethers.BigNumber.from('30'); // 30 seconds
+const MAX_DURATION = ethers.BigNumber.from('120'); // two minutes
+const PLEDGE_AMOUNT = ethers.utils.parseEther('1');
 
 const deployCrowdfund: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
@@ -23,33 +23,33 @@ const deployCrowdfund: DeployFunction = async function (
   const chainId = network.config.chainId;
 
   if (chainId == 31337) {
-    
     const waitBlockConfirmations = developmentChains.includes(network.name)
-    ? 1
-    : VERIFICATION_BLOCK_CONFIRMATIONS;
+      ? 1
+      : VERIFICATION_BLOCK_CONFIRMATIONS;
 
-  log('----------------------------------------------------');
-  const initArgs: any[] = [
-    // mockToken address
-    MIN_DURATION,
-    MAX_DURATION
-  ];
-  const crowdfund = await deploy('Crowdfund', {
-    from: deployer,
-    log: true,
-    waitConfirmations: waitBlockConfirmations,
-  });
+    log('----------------------------------------------------');
+    const initArgs: any[] = [
+      // mockToken address
+      MIN_DURATION,
+      MAX_DURATION
+    ];
+    const crowdfund = await deploy('Crowdfund', {
+      from: deployer,
+      log: true,
+      waitConfirmations: waitBlockConfirmations
+    });
 
-  // Verify the deployment
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
-    log('Verifying...');
-    await verify(crowdfund.address, []);
+    // Verify the deployment
+    if (
+      !developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+    ) {
+      log('Verifying...');
+      await verify(crowdfund.address, []);
+    }
+    log('----------------------------------------------------');
   }
-  log('----------------------------------------------------');
-  };
-  
+};
+
 export default deployCrowdfund;
 deployCrowdfund.tags = ['all', 'crowdfund'];
