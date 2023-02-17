baef85ee9492bc5ddf5ee9ebb5c939ad13480dfb
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-16 19:55:31 -0500

    updated hardhat config and .env.example

--- deploy/01_deployCrowdfund.ts
+++ deploy/01_deployCrowdfund.ts
@@ -7,10 +7,10 @@ const developmentChains = ['hardhat', 'localhost'];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 import verify from '../utils/verify';
+import { MockToken } from '../typechain-types/contracts/MockToken';
 
 const MIN_DURATION: BigNumber = ethers.BigNumber.from('30'); // 30 seconds
 const MAX_DURATION = ethers.BigNumber.from('120'); // two minutes
-const PLEDGE_AMOUNT = ethers.utils.parseEther('1');
 
 const deployCrowdfund: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
@@ -18,21 +18,52 @@ const deployCrowdfund: DeployFunction = async function (
   // @ts-ignore
   const { deployments, getNamedAccounts, network, ethers } = hre;
   const { deploy, log } = deployments;
-  const { deployer, pledger1, pledger2 } = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
 
   const chainId = network.config.chainId;
+  let mockToken;
 
   if (chainId == 31337) {
+    mockToken: MockToken = await ethers.getContract('MockToken');
+
     const waitBlockConfirmations = developmentChains.includes(network.name)
       ? 1
       : VERIFICATION_BLOCK_CONFIRMATIONS;
 
     log('----------------------------------------------------');
-    const initArgs: any[] = [
-      // mockToken address
-      MIN_DURATION,
-      MAX_DURATION
-    ];
+
+    const initArgs: any[] = [mockToken.address, MIN_DURATION, MAX_DURATION];
+    console.log(initArgs);
+
+    const crowdfund = await deploy('Crowdfund', {
+      from: deployer,
+      log: true,
+      waitConfirmations: waitBlockConfirmations
+    });
+
+    // Verify the deployment
+    if (
+      !developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+    ) {
+      log('Verifying...');
+      await verify(crowdfund.address, []);
+    }
+    log('----------------------------------------------------');
+  } else {
+    const MockToken: MockToken__factory = await ethers.getContractFactory(
+      'MockToken'
+    );
+    const mockToken: MockToken = await MockToken.deploy();
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+      ? 1
+      : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+    log('----------------------------------------------------');
+
+    const initArgs: any[] = [mockToken.address, MIN_DURATION, MAX_DURATION];
+
     const crowdfund = await deploy('Crowdfund', {
       from: deployer,
       log: true,
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,9 +17,6 @@ const MAINNET_RPC_URL =
   process.env.MAINNET_RPC_URL ||
   process.env.ALCHEMY_MAINNET_RPC_URL ||
   'https://eth-mainnet.alchemyapi.io/v2/your-api-key';
-const SEPOLIA_RPC_URL =
-  process.env.SEPOLIA_RPC_UR ||
-  'https://eth-rinkeby.alchemyapi.io/v2/your-api-key';
 const GOERELI_RPC_URL =
   process.env.GOERELI_RPC_UR ||
   'https://eth-kovan.alchemyapi.io/v2/your-api-key';
@@ -28,65 +25,54 @@ const POLYGON_MAINNET_RPC_URL =
   'https://polygon-mainnet.alchemyapi.io/v2/your-api-key';
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY =
-  process.env.ETHERSCAN_API_KEY || 'Your etherscan API key';
-const POLYGONSCAN_API_KEY =
-  process.env.POLYGONSCAN_API_KEY || 'Your polygonscan API key';
-const REPORT_GAS = process.env.REPORT_GAS || false;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'your-api-key';
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || 'your-api-key';
+const REPORT_GAS = Boolean(process.env.REPORT_GAS) || false;
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
-      // // If you want to do some forking, uncomment this
-      // forking: {
-      //   url: MAINNET_RPC_URL
-      // }
+      forking: {
+        url: MAINNET_RPC_URL
+      },
       chainId: 31337
     },
     localhost: {
       chainId: 31337
     },
-    // sepolia: {
-    //   url: SEPOLIA_RPC_URL,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    //   saveDeployments: true,
-    //   chainId: 42
-    // },
-    // goerli: {
-    //   url: GOERELI_RPC_URL,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    //   saveDeployments: true,
-    //   chainId: 4
-    // },
-    // mainnet: {
-    //   url: MAINNET_RPC_URL,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    //   saveDeployments: true,
-    //   chainId: 1
-    // },
-    // polygon: {
-    //   url: POLYGON_MAINNET_RPC_URL,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    //   saveDeployments: true,
-    //   chainId: 137
-    // }
+    goerli: {
+      url: GOERELI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 5
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 1
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 137
+    }
   },
   etherscan: {
     // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
     apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
       goerli: ETHERSCAN_API_KEY,
       polygon: POLYGONSCAN_API_KEY
     }
   },
   gasReporter: {
-    enabled: false,
+    enabled: REPORT_GAS,
     currency: 'USD',
     outputFile: 'gas-report.txt',
-    noColors: true
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
   },
   namedAccounts: {
     deployer: {
