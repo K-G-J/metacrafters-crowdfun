ada272ecdac3f04edf7da7222a425b7aec1d46f7
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-20 21:03:41 -0500

    trying console prompts in launch script

--- scripts/launch.ts
+++ scripts/launch.ts
@@ -1,11 +1,11 @@
+import * as readline from 'readline';
 import { ethers, getNamedAccounts, network } from 'hardhat';
 import { Crowdfund } from '../typechain-types';
 import { BigNumber } from 'ethers';
 import { networkConfig } from '../helper-hardhat-config';
 
 const chainId = network.config.chainId;
-const CAMPAIGN_GOAL: BigNumber = ethers.utils.parseEther('5');
-const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+let campaignGoal: BigNumber, account: string;
 
 // yarn hardhat run scripts/launch.ts --network localhost
 
@@ -19,17 +19,18 @@ export default async function launch(
 
     const crowdfund: Crowdfund = await ethers.getContract('Crowdfund');
 
-    const minDuation =
-      networkConfig[network.config.chainId!]['min_duration'].toNumber();
-    const maxDuation =
-      networkConfig[network.config.chainId!]['max_duration'].toNumber();
+    const currentTimestampInSeconds = ethers.BigNumber.from(
+      Math.round(Date.now() / 1000)
+    );
+    const minDuation = networkConfig[network.config.chainId!]['min_duration'];
+    const maxDuation = networkConfig[network.config.chainId!]['max_duration'];
 
     await crowdfund
       .connect(signer)
       .launch(
         campaignGoal,
-        currentTimestampInSeconds + minDuation,
-        currentTimestampInSeconds + maxDuation
+        currentTimestampInSeconds.add(minDuation),
+        currentTimestampInSeconds.add(maxDuation)
       );
 
     console.log(
@@ -41,18 +42,10 @@ export default async function launch(
     const crowdfund: Crowdfund = await ethers.getContract('Crowdfund');
     const signer = ethers.provider.getSigner(account!);
 
-    const minDuation =
-      networkConfig[network.config.chainId!]['min_duration'].toNumber();
-    const maxDuation =
-      networkConfig[network.config.chainId!]['max_duration'].toNumber();
+    const startAt = await getStartAt();
+    const endAt = await getEndAt();
 
-    await crowdfund
-      .connect(signer)
-      .launch(
-        campaignGoal,
-        currentTimestampInSeconds + minDuation,
-        currentTimestampInSeconds + maxDuation
-      );
+    await crowdfund.connect(signer).launch(campaignGoal, startAt, endAt);
 
     console.log(
       `\nCampaign launched!\nCampaign Goal: ${ethers.utils.formatEther(
@@ -62,16 +55,99 @@ export default async function launch(
   }
 }
 
-launch(CAMPAIGN_GOAL)
-  .then(() => process.exit(0))
-  .catch((error) => {
-    const reason = error.reason
-      .replace(
-        'Error: VM Exception while processing transaction: reverted with reason string ',
-        ''
-      )
-      .replace(/[']/g, '');
-    reason.replace("''", '');
-    console.log(`\n\n${reason}\n\n`);
-    process.exit(1);
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+export const getCampaignGoal = async (): Promise<BigNumber> => {
+  let goal: BigNumber = ethers.BigNumber.from(0);
+  rl.question('What is the campaign goal?  ', (answer) => {
+    while (!isNaN(parseInt(answer))) {
+      getCampaignGoal();
+    }
+    goal = ethers.BigNumber.from(answer);
+    rl.close();
   });
+  return goal;
+};
+
+export const getAccount = async (): Promise<string> => {
+  let account: string = '';
+  rl.question('What is your address?  ', (answer) => {
+    while (!/^(0x)?[0-9a-f]{40}$/i.test(answer)) {
+      console.log('\nThat is not a valid address\n');
+      getAccount();
+    }
+    account = answer;
+    rl.close();
+  });
+  return account;
+};
+
+export const getStartAt = async (): Promise<BigNumber> => {
+  let startAt: BigNumber = ethers.BigNumber.from(0);
+  rl.question('When does the campaign start?  ', (answer) => {
+    while (!isNaN(parseInt(answer))) {
+      getStartAt();
+    }
+    startAt = ethers.BigNumber.from(answer);
+    rl.close();
+  });
+  return startAt;
+};
+
+export const getEndAt = async (): Promise<BigNumber> => {
+  let endAt: BigNumber = ethers.BigNumber.from(0);
+  rl.question('When does the campaign end?  ', (answer) => {
+    while (!isNaN(parseInt(answer))) {
+      getEndAt();
+    }
+    endAt = ethers.BigNumber.from(answer);
+    rl.close();
+  });
+  return endAt;
+};
+
+(() => {
+  rl.question('Are you on localhost? [y/n] ', (answer) => {
+    switch (answer.toLowerCase()) {
+      case 'y':
+        campaignGoal = await getCampaignGoal();
+        launch(campaignGoal)
+          .then(() => process.exit(0))
+          .catch((error) => {
+            const reason = error.reason
+              .replace(
+                'Error: VM Exception while processing transaction: reverted with reason string ',
+                ''
+              )
+              .replace(/[']/g, '');
+            reason.replace("''", '');
+            console.log(`\n\n${reason}\n\n`);
+            process.exit(1);
+          });
+        break;
+      case 'n':
+        account = await getAccount();
+        campaignGoal = await getCampaignGoal();
+        launch(campaignGoal, account)
+          .then(() => process.exit(0))
+          .catch((error) => {
+            const reason = error.reason
+              .replace(
+                'Error: VM Exception while processing transaction: reverted with reason string ',
+                ''
+              )
+              .replace(/[']/g, '');
+            reason.replace("''", '');
+            console.log(`\n\n${reason}\n\n`);
+            process.exit(1);
+          });
+        break;
+      default:
+        console.log('Invalid answer');
+    }
+    rl.close();
+  });
+})();
